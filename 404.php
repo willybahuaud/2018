49103d7a9c5e847ea2210da6033beced2d88ae79
Bahuaud Willy <>
2017-12-01 12:50:52 +0100

    La 404, évidemment…

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+get_header();
+
+echo '<div class="content-wrapper content-wrapper-page description"><div class="text">
+    <h2>' . __( 'Oups !', 'dd8' ) . '</h2>
+    <p>' . __( 'Restez calme, c’est juste une 404…', 'dd8' ) . '</p>
+    <p><img src="https://media.giphy.com/media/13HgwGsXF0aiGY/giphy.gif" alt="You break the internet"/></p>
+    </div></div>';
+
+get_footer();
