c9cc5699e80ca00acf3eaaccc9230a8f889798f3
Bahuaud Willy <>
2017-12-01 16:01:53 +0100

    debug konami

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,7 +85,8 @@ function getRandomString( count ) {
 // https://codepen.io/NyX/pen/mJezOE
 
 var k = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
-    n = 0;
+    n = 0,
+    audio = false;
 $(document).keydown(function (e) {
     if (e.keyCode === k[n++]) {
         if (n === k.length) {
